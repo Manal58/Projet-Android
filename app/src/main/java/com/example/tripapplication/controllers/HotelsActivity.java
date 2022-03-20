53788080d12753eac71bb1a0788169e78456e2ef
aicha-mudr <moudar_aicha@um5.ac.ma>
2022-03-20 22:49:20 +0100

    initial commit

---
+++ app/src/main/java/com/example/tripapplication/controllers/HotelsActivity.java
@@ -0,0 +1,16 @@
+package com.example.tripapplication.controllers;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.tripapplication.R;
+
+public class HotelsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hotels2);
+    }
+}
---
+++ app/src/main/java/com/example/tripapplication/controllers/RestaurantsActivity.java
@@ -0,0 +1,15 @@
+package com.example.tripapplication.controllers;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.example.tripapplication.R;
+
+public class RestaurantsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restaurants);
+    }
+}
